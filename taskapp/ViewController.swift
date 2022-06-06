f12f15f3c39acb0c2709b4e2602aee5e4744abe7
shiyunichi <nuksga@nifty.com>
2022-06-07 02:58:04 +0900

    課題前まで終了

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
@@ -25,6 +26,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.fillerRowHeight = UITableView.automaticDimension
         tableView.delegate = self
         tableView.dataSource = self
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
     }
     
     //データの数（＝セルの数）を返すメソッド
@@ -65,7 +68,55 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //Deleteボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
+        if editingStyle == .delete{
+            //削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+            //ローカル通知をキャンセルする。
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            
+            //データベースから削除する。
+            try! realm.write{
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            //未知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests{(requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/---------------")
+                    print(request)
+                    print("/---------------")
+                }
+            }
+        }
+   
+}
+    
+    //segueで画面遷移する時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue"{
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0{
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
 }
 
